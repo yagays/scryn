fa7bf02b6b8e02fd3be38f248f18e276f515c480
Yuki Okuda <y.okuda@dr-ubie.com>
2022-02-11 10:02:28 +0900

    use worker name

--- scryn/scryn.py
+++ scryn/scryn.py
@@ -1,22 +1,33 @@
-from typing import List
+from typing import List, Dict
+from dataclasses import dataclass, field
 
 from scryn.task import Task
+
+
+@dataclass
+class Annotation:
+    worker_name: str
+    annotations: List = field(default_factory=list)
 
 
 class Scryn:
     def __init__(self, tasks: List[Task]) -> None:
         self.tasks = tasks
         self.current_task_id = 0
-        self.annotation = {}
+        self.worker2annotation = {}
 
-    def get_task(self) -> Task:
+    def get_task(self, worker: str) -> Task:
+        if worker not in self.worker2annotation:
+            # initialize worker's Annotation
+            self.worker2annotation[worker] = Annotation(worker_name=worker)
+
         next_task = self.tasks[self.current_task_id]
         self.current_task_id += 1
 
         return next_task
 
-    def annotate(self, task: Task, annotation: str, worker=None) -> None:
-        self.annotation[task.task_id] = annotation
+    def annotate(self, task: Task, annotation: str, worker: str) -> None:
+        self.worker2annotation[worker].annotations.append({task.task_id: annotation})
 
-    def show_annotation(self, worker=None) -> None:
-        print(self.annotation)
+    def show_annotation(self, worker: str) -> None:
+        print(self.worker2annotation[worker])
--- tools/run_cli.py
+++ tools/run_cli.py
@@ -10,13 +10,13 @@ scryn = Scryn(tasks=tasks)
 num_annotate = 1000
 
 for i in range(num_annotate):
-    task = scryn.get_task()
+    task = scryn.get_task(worker)
     try:
         answer = input(task.text + ": ")
-        scryn.annotate(task, answer)
+        scryn.annotate(task, answer, worker=worker)
         print(answer)
     except KeyboardInterrupt:
         break
 
 print()
-scryn.show_annotation()
+scryn.show_annotation(worker=worker)
